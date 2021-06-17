b86189115b5e78a17c10ac6d97e694627eb78f19
AdamElderfield <58347187+AdamElderfield@users.noreply.github.com>
2021-06-18 07:05:28 +1000

    Testing filter on higher dimension matrices

--- KF exact initialisation.jl
+++ KF exact initialisation.jl
@@ -16,7 +16,7 @@ import XLSX
 # Function 2 - Log Likelihood estimation
 # Function 3 - Kalman smoother recursions
 # ---------------------------------------------------------
-
+# ALT-J then ALT-O to resart REPL
 
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # Mod
@@ -25,10 +25,10 @@ import XLSX
 
 mutable struct dlm  
     FF
-    A
     H
     Q
     R
+    A
     k
     n
     cur_ξ_hat
@@ -49,12 +49,12 @@ end
  #  y_t = A'x_t + H'ξ_t + Rv_t 
 
  # TODO: Expand to inlcude x and y?
-function dlm(A, FF, Q, R, H)
+function dlm(FF,H, Q, R, A)
     k = size(H, 2)   # Number of y variables (columns of H as it is transposed)
     n = size(H, 1)   # Number of states (row of H as it is transposed
     ξ = n == 1 ? zero(eltype(FF)) : zeros(n)                    # IF n == 1 then 0, else zeros[n,1]
     Σ = n == 1 ? zero(eltype(FF)) : zeros(n, n)                 # IF n == 1 then 0, else zeros[n,n]   
-    return dlm(A, FF, Q, R, H, k, n, ξ, Σ)
+    return dlm(FF, H, Q, R, A, k, n, ξ, Σ)
 end
 
 
@@ -75,7 +75,7 @@ end
 # Assumes states have been initialised
 
 function KalmanFilter(m::dlm,y::AbstractArray, x = "NA")
-   # m =mod1
+    #m =mod1
     FF, A, H, Q, R = m.FF, m.A, m.H, m.Q, m.R
       
     k, n, = m.k, m.n
@@ -87,12 +87,11 @@ function KalmanFilter(m::dlm,y::AbstractArray, x = "NA")
     # Number of time periods
     T = size(y,1)
 
+    # Filtered mean and variance 
     ξ_f = zeros(T,n)
     Σ_f = zeros(n,n,T)
     
-   # ξ_f[1,n] = m.cur_ξ_hat
-   # Σ_f[n,n,1] = m.cur_Σ_hat
-
+    # Predicted mean and variance 
     ξ_p = zeros(T,n)
     Σ_p = zeros(n,n,T)
 
@@ -105,7 +104,7 @@ function KalmanFilter(m::dlm,y::AbstractArray, x = "NA")
         # ******************************
         # Prediction step 
         # ******************************
-        
+       # t = 1
         #--------------------------------
         # Conditional initialses the filter with priors if t = 1
         #--------------------------------
@@ -128,6 +127,7 @@ function KalmanFilter(m::dlm,y::AbstractArray, x = "NA")
             reshape(y[t], m.k, 1)
         end
 
+        # Matrix size error here!
         # Prediction error
         Hp = transpose(H)
         Ap = transpose(A)
@@ -157,6 +157,14 @@ function KalmanFilter(m::dlm,y::AbstractArray, x = "NA")
 end
 
 
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Kalman smoother
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Tests
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
 # Nile river data for testing
@@ -168,13 +176,13 @@ dat =  DataFrame(XLSX.readtable("C:/Users/aelde/OneDrive/Documents/GitHub/Tutori
  #  y_t = A'x_t + H'ξ_t + Rv_t 
 
  A = [0.0]
- H = [1.0]
- FF = [1.0]
- Q = [1469.1]
+ H = [1.0; 0.0]
+ FF = [1.0 1.0;0.0  1.0]
+ Q = [1469.1 0.0; 0.0   100.0]
  R = [15099.0]
 
-mod1= dlm(FF,A,H,Q,R)
-initialise!(mod1,0,100000)
+mod1= dlm(FF,H,Q,R,A)
+initialise!(mod1,[0.0 ;0.0],[100000 0; 0 1000])
 a =KalmanFilter(mod1,dat[:,1])
 a[3]
 plot([a[3],y])
